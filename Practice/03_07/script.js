dbe411940aa0c092beb7a3fd9250bd9aea0e83e4
MehmetMSahin <120316756+MelihMSahin@users.noreply.github.com>
2026-01-20 16:06:53 +0000

    Practice 3.7 headphone

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,22 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const headphone = {
+  name: "wh1000 x6",
+  colour: "black",
+  isBlueTooth: true,
+  current_music: null,
+  playMusic: function (musicToPlay) {
+    this.current_music = musicToPlay;
+  },
+};
+
+const headphone_case = {
+  headphone: headphone,
+  colour: "grey",
+  is_open: false,
+  toggleLid: function () {
+    this.is_open = !this.is_open;
+  },
+};
